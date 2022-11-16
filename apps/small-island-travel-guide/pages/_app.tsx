7be43344f486c5bc68b311653315b1a76d07242a
machelslackatnewsuk <machelsandile.slack@news.co.uk>
2022-11-16 08:00:44 +0000

    removed comments

--- apps/small-island-travel-guide/pages/_app.tsx
+++ apps/small-island-travel-guide/pages/_app.tsx
@@ -16,12 +16,3 @@ function CustomApp({ Component, pageProps }: AppProps) {
 }
 
 export default CustomApp;
-
-///https://enqueuezero.com/airbnb-architecture.html#data-tier
-//https://medium.com/@narengowda/netflix-system-design-dbec30fede8d
-//https://github.com/donnemartin/system-design-primer/blob/master/solutions/system_design/scaling_aws/README.md
-//https://netflixtechblog.com/open-sourcing-zuul-2-82ea476cb2b3
-//https://cloud.google.com/blog/topics/developers-practitioners/implementing-leader-election-google-cloud-storage
-//https://levelup.gitconnected.com/9-projects-you-can-do-to-become-a-front-end-master-in-2023-a4389153148c
-//https://medium.com/@wastecleaner/frontend-system-design-framework-5ba6e075b3b2
-//https://medium.com/tech-career/l8-explains-career-of-software-engineers-5969a8ae0351
--- libs/small-island-travel-guide-my-sql-database/src/index.ts
+++ libs/small-island-travel-guide-my-sql-database/src/index.ts
@@ -4,49 +4,11 @@ const con = mysql.createConnection({
   host: 'database-2-instance-1.chv598kqemzh.eu-west-1.rds.amazonaws.com',
   user: 'admin',
   password: '321123ETz$',
-  port: 3306,
+  port: 3306
 });
 
 con.connect(function (err) {
   if (err) throw err;
   console.log('Connected!');
-//    con.query('CREATE DATABASE IF NOT EXISTS small-island-travel-guide;');
-//     con.query('USE small-island-travel-guide;');
-//     con.query(
-//       'CREATE TABLE IF NOT EXISTS locations(location_id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(location_id));',
-//       function (error, result, fields) {
-//         console.log(result);
-//       }
-//     );
-//     con.query(
-//       'CREATE TABLE IF NOT EXISTS hosts(host_id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(host_id)), FOREIGN KEY (location_id) REFERENCES locations(location_id);;',
-//       function (error, result, fields) {
-//         console.log(result);
-//       }
-//     );
-//     con.query(
-//       'CREATE TABLE IF NOT EXISTS listings(listing_id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(listing_id)), FOREIGN KEY (host_id) REFERENCES hosts(host_id);',
-//       function (error, result, fields) {
-//         console.log(result);
-//       }
-//     );
-//     con.query(
-//       'CREATE TABLE IF NOT EXISTS bookings(booking_id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(booking_id)), FOREIGN KEY (listing_id) REFERENCES listings(listing_id),FOREIGN KEY (guest_id) REFERENCES guests(guest_id);',
-//       function (error, result, fields) {
-//         console.log(result);
-//       }
-//     );
-//     con.query(
-//       'CREATE TABLE IF NOT EXISTS amenities(amenity_id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(amenity_id)), FOREIGN KEY (listing_id) REFERENCES listings(listing_id);',
-//       function (error, result, fields) {
-//         console.log(result);
-//       }
-//     );
-//     con.query(
-//       'CREATE TABLE IF NOT EXISTS guests(guest_id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(guest_id));',
-//       function (error, result, fields) {
-//         console.log(result);
-//       }
-//     );
   con.end();
 });
